8d56e58a27714f4b2b3cf877984bb1c316856a42
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-02-18 10:45:15 +0100

    Finished Posts List and Post details

--- app/Post.php
+++ app/Post.php
@@ -32,4 +32,13 @@ class Post extends Model
 				]
 		];
 	}
+
+	public function user(){
+		return $this->belongsTo(User::class);
+	}
+
+	public function getGetExcerptAttribute(){
+		return substr($this->body, 0, 140);
+	}
+
 }
