eabec7be27cf6d1ecd15a4071e49936227cef0af
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-02-19 11:38:27 +0100

    Finished TDD Show method

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,8 +5,9 @@
 use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
-    return [
-        //
-    ];
+$factory->define(Post::class, function (Faker $faker)
+{
+	return [
+		'title' => $faker->sentence()
+	];
 });
