f237b5bf8dc942ae8657d40f850b7e3ad6dc4a74
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-02-25 16:02:43 +0100

    - Created index View - Added Routes for notes Resources and added Notes link in Layout

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,27 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+/* Route::get('/', function () {
+	return Inertia::render('Welcome', [
+		'canLogin' => Route::has('login'),
+		'canRegister' => Route::has('register'),
+		'laravelVersion' => Application::VERSION,
+		'phpVersion' => PHP_VERSION,
+	]);
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+	return Inertia::render('Dashboard');
 })->name('dashboard');
+ */
+
+Route::view('/','index');
+
+Route::get('dashboard', [
+	App\Http\Controllers\PageController::class,'dashboard'
+	])
+	->middleware('auth:sanctum')
+	->name('dashboard');
+
+Route::resource('notes',App\Http\Controllers\NoteController::class)
+	->middleware('auth:sanctum');
