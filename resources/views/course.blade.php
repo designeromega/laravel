7d31817a03a723b5d2b442891190b9efccc9d415
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-02-25 10:25:30 +0100

    Added Courses Details

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -7,7 +7,17 @@
 				<li class="font-medium text-sm text-gray-400 uppercase mb-4">
 					Contenido
 				</li>
-				----
+				@foreach($course->posts as $post)
+				<li class="flex items-center text-gray-600 mt-2">
+					{{ $post->name }}
+					@if($post->free)
+						<span 
+							class="text-xs text-gray-500 font-semibold bg-gray-300 rounded-full ml-auto px-2">
+							Gratis
+						</span>
+					@endif
+				</li>
+				@endforeach
 			</ul>
 		</div>
 		<div class="text-gray-700 col-span-2">
