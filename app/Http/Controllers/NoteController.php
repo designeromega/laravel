96f6ac41bf289881f1a9d59a01d8b133f1bf5859
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-02-25 19:32:12 +0100

    Added Dinamic search to filter the notes in the Notes index

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -11,12 +11,15 @@ class NoteController extends Controller
 	/**
 	 * Display a listing of the resource.
 	 *
+	 * @param  \Illuminate\Http\Request  $request
 	 * @return \Illuminate\Http\Response
 	 */
-	public function index()
+	public function index(Request $request)
 	{
 		return Inertia::render('Notes/Index', [
-			'notes' => Note::latest()->get()
+			'notes' => Note::latest()
+				->where('excerpt', 'LIKE',"%$request->q%")
+				->get()
 		]);
 	}
 
